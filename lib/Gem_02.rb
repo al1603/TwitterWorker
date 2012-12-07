7c5689172f108c408a3c46aaaf6f5d3ff866cd38
al <alShamshev@yandex.ru>
2012-12-07 05:36:43 -0800

    creating gem

--- lib/Gem_02.rb
+++ lib/Gem_02.rb
@@ -2,6 +2,6 @@ require "Gem_02/version"
 
 module Gem02
   autoload :SumClass, 'Gem_02/sum_class'
-  autoload :PointGem, 'Gem_02/point_gem'
+  #autoload :PointGem, 'Gem_02/sum_class'
 
 end
--- lib/Gem_02/sum_class.rb
+++ lib/Gem_02/sum_class.rb
@@ -7,15 +7,5 @@
 
   end
 
-  class PointGem
 
-    def initialize()
-      puts 'calling initialize'
-    end
-    # To change this template use File | Settings | File Templates.
-    def self.test_func
-      puts "calling of test_func"
-    end
-
-  end
 #end
